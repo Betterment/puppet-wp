52f99381ce489b8f25c4cb895ebb5b4c7f009dad
Sarah Whinnem <sarah@betterment.com>
2016-04-13 11:33:33 -0400

    pass location into core download

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,46 +1,46 @@
 define wp::site (
-	$location = $title,
-	$url,
-	$siteurl = $url,
-	$sitename       = 'WordPress Site',
-	$admin_user     = 'admin',
-	$admin_email    = 'admin@example.com',
-	$admin_password = 'password',
-	$network        = false,
-	$subdomains     = false
+    $location = $title,
+    $url,
+    $siteurl = $url,
+    $sitename       = 'WordPress Site',
+    $admin_user     = 'admin',
+    $admin_email    = 'admin@example.com',
+    $admin_password = 'password',
+    $network        = false,
+    $subdomains     = false
 ) {
-	include wp::cli
+    include wp::cli
 
-	if ( $network == true ) and ( $subdomains == true ) {
-		$install = "multisite-install --subdomains --url='$url'"
-	}
-	elsif ( $network == true ) {
-		$install = "multisite-install --url='$url'"
-	}
-	else {
-		$install = "install --url='$url'"
-	}
+    if ( $network == true ) and ( $subdomains == true ) {
+        $install = "multisite-install --subdomains --url='$url'"
+    }
+    elsif ( $network == true ) {
+        $install = "multisite-install --url='$url'"
+    }
+    else {
+        $install = "install --url='$url'"
+    }
 
-	exec {"download wp core":
-		command => "/usr/local/bin/wp core download",
-		cwd => $location,
-		creates => "$location/wp-config-sample.php",
-		require => [ Class['wp::cli'] ],
-	}
+    exec {"download core $location":
+        command => "/usr/local/bin/wp core download",
+        cwd => $location,
+        creates => "$location/wp-config-sample.php",
+        require => [ Class['wp::cli'] ],
+    }
 
-	exec {"wp install $location":
-		command => "/usr/local/bin/wp core $install --title='$sitename' --admin_email='$admin_email' --admin_name='$admin_user' --admin_password='$admin_password'",
-		cwd => $location,
-		require => [ Class['wp::cli'] ],
-		unless => '/usr/local/bin/wp core is-installed'
-	}
+    exec {"wp install $location":
+        command => "/usr/local/bin/wp core $install --title='$sitename' --admin_email='$admin_email' --admin_name='$admin_user' --admin_password='$admin_password'",
+        cwd => $location,
+        require => [ Class['wp::cli'] ],
+        unless => '/usr/local/bin/wp core is-installed'
+    }
 
-	if $siteurl != $url {
-		wp::option {"wp siteurl $location":
-			location => $location,
-			ensure => "equal",
-			key => "siteurl",
-			value => $siteurl
-		}
-	}
+    if $siteurl != $url {
+        wp::option {"wp siteurl $location":
+            location => $location,
+            ensure => "equal",
+            key => "siteurl",
+            value => $siteurl
+        }
+    }
 }
